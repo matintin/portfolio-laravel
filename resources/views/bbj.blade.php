0e9ba83f36ebf410411d5e3bc8a5b3003290da19
Martin Cho <chadcho82@hotmail.com>
2015-10-07 21:48:47 +1300

    bbj change

--- resources/views/bbj.blade.php
+++ resources/views/bbj.blade.php
@@ -4,39 +4,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<style>
-		body {
-			width: 95vw;
-			height:200vh;
-			background-image: url(./img/bbj_home.png);
-			background-repeat: no-repeat;
-			background-size: cover;
-			overflow: scroll;
+	body {
+		margin: 0;
+		padding:0;
+	}
+		img {
+			
+			width: 100%;
+			height: 100%;
 		}
-		@media only screen and (min-width : 44em) {
-			body {
-				width: 100vw;
-				height: 400vh;
-			}
 		
-		}
-		@media only screen and (min-width : 64em) {
-			body {
-				width: 100vw;
-				height: 650vh;
-			}
-		
-		}
-		@media only screen and (min-width : 84em) {
-			body {
-				width: 100vw;
-				height: 1000vh;
-			}
-		
-		}
 	</style>
 	<title>BBJ</title>
 </head>
 <body>
-	
+	<img src="{{asset('img/bbj_home.png')}}" alt="">
 </body>
 </html>
